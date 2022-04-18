7c60f1b87dbfead68854e4f78c72bfbafb31f67f
Joaquin <jabantoloza99@gmail.com>
2022-04-18 01:20:50 -0500

    primer commit

---
+++ src/app/gifs/gifs-pages/gifs-pages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-pages',
+  templateUrl: './gifs-pages.component.html',
+  styles: [
+  ]
+})
+export class GifsPagesComponent{ }
